707ba8639678be9e21e1d1c4d9c47fc6302ca071
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 <fabio_inacio@hotmail.com>
2025-01-20 15:28:46 -0300

    Card - Implementa uma função para agregar amigos

--- app.js
+++ app.js
@@ -2,3 +2,35 @@
 
 //Card - Crie um array para armazenar os nomes
 let amigosArray = [];
+
+//Card - Implementa uma função para agregar amigos
+
+function adicionarAmigo() {
+//variavel para pegar o valor do nome do amigo
+let amigo = document.getElementById('amigo');
+
+//checar se o campo está preenchido
+if (amigo.value ==''){
+    alert('Por favor, insira um nome.');
+    return;
+}
+
+
+if (amigosArray.includes(amigo.value)){
+    alert('Adicione um nome diferente do ja adicionado');
+    return;
+}
+
+//variável para colocar o nome do amigo na lista
+let listaAmigos = document.getElementById('listaAmigos');
+
+//função que coloca o nome dos amigos digitados na lista
+amigosArray.push(amigo.value);
+if (listaAmigos.textContent == '') {
+    listaAmigos.textContent = amigo.value;
+} else {
+    listaAmigos.textContent = listaAmigos.textContent + ', ' + amigo.value;
+}
+amigo.value = ''
+}
+
