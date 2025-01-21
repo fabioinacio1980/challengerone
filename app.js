d6fd7ea2db01dbfb1c9920ab49762fb0223823d1
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 <fabio_inacio@hotmail.com>
2025-01-21 09:22:35 -0300

    Card - Implementa uma função para sortear os amigos

--- app.js
+++ app.js
@@ -41,3 +41,38 @@ function limparLista() {
     document.getElementById('resultado').innerHTML = '';
 }
 
+//Card - Implementa uma função para sortear os amigos
+    //função embaralhar
+    //função para embaralhar a lista, para que não seja possível o primeiro pegar o segundo e assim por diante
+function embaralha(lista) {
+
+    for (let indice = lista.length; indice; indice--) {
+
+        const indiceAleatorio = Math.floor(Math.random() * indice);
+
+        // atribuição via destructuring
+        [lista[indice - 1], lista[indiceAleatorio]] =
+            [lista[indiceAleatorio], lista[indice - 1]];
+    }
+}
+
+    //função para Sortear amigo e imprimir na página HTML
+function sortearAmigo() {
+    //informar pelo menor 4 amigos
+    if (amigosArray.length < 4){
+        alert('Adicione pelo menos 4 amigos');
+        return;
+    }
+
+    embaralha(amigosArray);
+    let listaSorteio = document.getElementById('resultado');
+    //percorre a lista (array)
+    for (let i = 0; i < amigosArray.length; i++) {
+        if (i == amigosArray.length - 1) {
+            listaSorteio.innerHTML = listaSorteio.innerHTML + amigosArray[i] + ' --> ' + amigosArray[0] + '<br>';
+        } else {
+             listaSorteio.innerHTML = listaSorteio.innerHTML + amigosArray[i] + ' --> ' + amigosArray[i + 1] + '<br>';
+        }
+           
+    }
+}
